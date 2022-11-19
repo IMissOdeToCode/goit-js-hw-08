fa732ff0893dd84f3c89adb009da8f8c9f5942f3
Denys Konopatkin <k.denis.aa@gmail.com>
2022-11-19 15:14:47 +0200

    task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// import { galleryItems } from './gallery-items.js';
+// Change code below this line
+// references
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+const getItemTemplate = Item => {
+  const { preview, original, description } = Item;
+
+  // prettier-ignore
+  const templateString =
+        `<a class="gallery__item" href="${original}" data-arr="test">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+
+  return templateString;
+};
+
+// making gallery markup
+const pictures = galleryItems.map(item => getItemTemplate(item));
+refs.gallery.insertAdjacentHTML('beforeend', pictures.join(''));
+
+// usage SimpleLightbox
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
