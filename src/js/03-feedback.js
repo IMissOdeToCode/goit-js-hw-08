fa40565ff14077b2f611048a8d94d4e5e3e5b724
Denys Konopatkin <k.denis.aa@gmail.com>
2022-11-20 18:02:55 +0200

    task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
+const refs = {
+  form: document.querySelector('form'),
+  input: document.querySelector('input'),
+  text: document.querySelector('textarea'),
+  button: document.querySelector('button'),
+};
+// console.log(refs);
 
+const template = {
+  email: '',
+  massage: '',
+};
+
+const payload = fetchData();
+loadData(payload);
+
+refs.form.addEventListener('submit', event => {
+  event.preventDefault();
+});
+
+refs.form.addEventListener('input', pushData);
+
+refs.button.addEventListener('click', () => {
+  console.log(`you send payload:`, payload);
+  refs.form.reset();
+});
+
+function pushData(event) {
+  switch (event.target.name) {
+    case 'email':
+      payload.email = event.target.value;
+      break;
+    case 'message':
+      payload.massage = event.target.value;
+      break;
+
+    default:
+      break;
+  }
+  localStorage.setItem('feedback-form-state', JSON.stringify(payload));
+}
+function fetchData() {
+  try {
+    return JSON.parse(localStorage.getItem('feedback-form-state')) || template;
+  } catch (error) {
+    console.log(`can't load data`);
+    return template;
+  }
+}
+
+function loadData(payload) {
+  refs.input.value = payload.email;
+  refs.text.value = payload.massage;
+}
