e7ffd422ee2f37eab8d97ab12ce65275bb876a24
Denys Konopatkin <k.denis.aa@gmail.com>
2022-11-20 12:30:03 +0200

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+// import lodash from 'lodash';
+const lodash = require('lodash');
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+let time = 0;
+time = localStorage.getItem('videoplayer-current-time') || 0;
+console.log(`last time you finished at ${time} sec`);
+player.setCurrentTime(time);
+
+player.on('play', function () {
+  console.log(`start play from ${time}`);
+});
+
+player.on('pause', function () {
+  console.log(`pause play at ${time}`);
+});
+
+player.on('timeupdate', lodash.throttle(onTimeUpdate, 5000));
+
+function onTimeUpdate(event) {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+}
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
